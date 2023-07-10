921a7c227d9a0d89085b2b47a2ba8208bde8f237
Dennis Muoki <muokid3@gmail.com>
2023-07-10 12:12:14 +0300

    added mongodb connection

--- util/database.js
+++ util/database.js
@@ -1,8 +1,21 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node_store', 'root', 'st34lthfr34k', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+// const sequelize = new Sequelize('node_store', 'root', 'st34lthfr34k', {
+//   dialect: 'mysql',
+//   host: 'localhost'
+// });
 
-module.exports = sequelize;
+const mongo = require("mongodb");
+const MongoClient = mongo.MongoClient;
+
+function mongoConnect(callback) {
+  MongoClient.connect(
+    "mongodb+srv://nodeuser:iv6ieHXdBfWww79R@cluster0.eyesdhv.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      callback(client);
+    })
+    .catch((err) => console.log(err));
+
+  }
+  module.exports = mongoConnect;
