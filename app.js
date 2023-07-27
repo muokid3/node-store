ee3395a486069e2a37befcfa7a563a56747554b3
Dennis Muoki <muokid3@gmail.com>
2023-07-27 08:11:56 +0300

    added csrf protection

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+
 
 const path = require("path");
 const errorController = require("./controllers/error");
@@ -16,6 +18,7 @@ const store = new MongoDBStore({
 });
 
 const app = express();
+const csrfProtection = csrf();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -37,6 +40,8 @@ app.use(
   })
 );
 
+app.use(csrfProtection);
+
 app.use((req, res, next) => {
   if (!req.session.loggedInUser) {
     return next();
@@ -47,6 +52,12 @@ app.use((req, res, next) => {
       next();
     })
     .catch((err) => console.log(err));
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.loggedInUser;
+  res.locals.csrfToken = req.csrfToken();
+  next();
 });
 
 app.use("/admin", adminRoutes);
--- controllers/admin.js
+++ controllers/admin.js
@@ -7,7 +7,6 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     editing: false,
-    isAuthenticated: req.session.loggedInUser,
   });
 };
 
@@ -51,7 +50,6 @@ exports.getEditProduct = (req, res, next) => {
         path: "/admin/edit-product",
         editing: editMode,
         product: product,
-        isAuthenticated: req.user,
       });
     })
     .catch((err) => console.log(err));
@@ -87,7 +85,6 @@ exports.getProducts = (req, res, next) => {
         prods: products,
         pageTitle: "Admin Products",
         path: "/admin/products",
-        isAuthenticated: req.user,
       });
     })
     .catch((err) => console.log(err));
