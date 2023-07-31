292382886a36e960e9d7ad0477092c805f4e2832
Dennis Muoki <muokid3@gmail.com>
2023-07-31 14:45:15 +0300

    password reset

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,13 @@
 const brcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
+const crypto = require("crypto");
+
 //const awsSdk = require("aws-sdk");
 let aws = require("@aws-sdk/client-ses");
 let { defaultProvider } = require("@aws-sdk/credential-provider-node");
 
 const User = require("../models/user");
+const { use } = require("../routes/admin");
 
 const ses = new aws.SES({
   region: process.env.AWS_DEFAULT_REGION,
@@ -91,38 +94,41 @@ exports.postSignup = (req, res, next) => {
 
         return res.redirect("/signup");
       }
-      return brcrypt.hash(pass, 12).then((hashedPass) => {
-        const user = new User({
-          name: name,
-          email: email,
-          password: hashedPass,
-          cart: { items: [] },
-        });
+      return brcrypt
+        .hash(pass, 12)
+        .then((hashedPass) => {
+          const user = new User({
+            name: name,
+            email: email,
+            password: hashedPass,
+            cart: { items: [] },
+          });
 
-        return user.save();
-      }).then(result => {
-              transporter
-                .sendMail({
-                  from: "shop@254fundi.com",
-                  to: email,
-                  subject: "Sign up Succcessful!",
-                  text: "You just signed up on the node store!",
-                  ses: {
-                    // optional extra arguments for SendRawEmail
-                    Tags: [
-                      {
-                        Name: "tag_name",
-                        Value: "tag_value",
-                      },
-                    ],
+          return user.save();
+        })
+        .then((result) => {
+          transporter
+            .sendMail({
+              from: "shop@254fundi.com",
+              to: email,
+              subject: "Sign up Succcessful!",
+              text: "You just signed up on the node store!",
+              ses: {
+                // optional extra arguments for SendRawEmail
+                Tags: [
+                  {
+                    Name: "tag_name",
+                    Value: "tag_value",
                   },
-                })
-                .then((result) => {
-                  console.log(result);
-                  return res.redirect("/login");
-                })
-                .catch((err) => console.log(err));
-      });
+                ],
+              },
+            })
+            .then((result) => {
+              console.log(result);
+              return res.redirect("/login");
+            })
+            .catch((err) => console.log(err));
+        });
     })
     .catch((err) => console.log(err));
 };
@@ -131,4 +137,124 @@ exports.postLogout = (req, res, next) => {
   req.session.destroy(() => {
     res.redirect("/");
   });
+};
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Reset Password",
+    errorMessage: message,
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect("/reset");
+    }
+
+    const token = buffer.toString("hex");
+
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash(
+            "error",
+            "We could not find a user with the given email in our records"
+          );
+          return res.redirect("/reset");
+        }
+
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 60 * 60 * 1000; //one hour from now
+        return user.save();
+      })
+      .then((result) => {
+        res.redirect("/reset");
+        transporter
+          .sendMail({
+            from: "shop@254fundi.com",
+            to: req.body.email,
+            subject: "Password Reset",
+            html: `
+             <p> You requested for a password reset.</>
+             <p>Please <a href="http://localhost:3000/reset/${token}">click here</a> to reset your password.</p>
+             <p>This link is only valid for 60 minutes </p>
+             `,
+            ses: {
+              // optional extra arguments for SendRawEmail
+              Tags: [
+                {
+                  Name: "tag_name",
+                  Value: "tag_value",
+                },
+              ],
+            },
+          })
+          .then((result) => {
+            console.log(result);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+};
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then((user) => {
+      let message = req.flash("error");
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render("auth/new-password", {
+        path: "/new-password",
+        pageTitle: "New Password",
+        errorMessage: message,
+        resetToken: token,
+        userId: user._id.toString(),
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const userId = req.body.userId;
+  const resetToken = req.body.resetToken;
+  const password = req.body.password;
+  let resetUser;
+
+  User.findOne({
+    _id: userId,
+    resetToken: resetToken,
+    resetTokenExpiration: { $gt: Date.now() },
+  })
+    .then((user) => {
+      resetUser = user;
+
+      return brcrypt.hash(password, 12);
+    })
+    .then((hashedPassword) => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+
+      return resetUser.save();
+    })
+    .then((result) => {
+      return res.redirect("login");
+    })
+    .catch((err) => console.log(err));
 };
