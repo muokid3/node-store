d2953fcbb9a10999a8b27c3f45bf68e1a77ec214
Dennis Muoki <muokid3@gmail.com>
2023-08-07 20:01:47 +0300

    gug fix

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,6 +3,7 @@ const Product = require("../models/product");
 const product = require("../models/product");
 const { validationResult } = require("express-validator");
 
+
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
